3cf4de74e08ab8d8189193d92e394cc1069b7a68
YJ Park <yjpark@gmail.com>
2024-04-09 20:22:29 +0800

    update doc

--- demo/emoji-browser/build.rs
+++ demo/emoji-browser/build.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use dioxus_daisyui::build::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=css/classes.rs");
     let current_dir = env::current_dir()?;
     let css_dir = Path::new(&current_dir).join("css");
     let classes_path = Path::new(&css_dir).join("classes.rs");
